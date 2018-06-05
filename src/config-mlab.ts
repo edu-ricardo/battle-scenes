6e8ffa9043ac7ce3ed8df43a023cbc7db918da91
eduardo.florencio <eduardo.florencio@millennium.com.br>
2018-06-05 01:16:06 -0300

    Iniciando Meus Cards

---
+++ src/config-mlab.ts
@@ -0,0 +1,4 @@
+export const dataMlab = {
+    baseMlabApi: 'https://api.mlab.com/api/1/databases/battlescenes',
+    apiKey: 'C8UDJttTmI0_KP63vZofTmSKYZ-Ae-wl'
+};
---
+++ src/providers/my-cards/my-cards.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { dataMlab } from "../../config-mlab";
+import { Observable } from 'rxjs/Rx';
+import { MyCard } from '../../models/my-card';
+import { CacheService } from 'ionic-cache';
+/*
+  Generated class for the MyCardsProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MyCardsProvider {
+
+  constructor(public http: HttpClient, public cache: CacheService) {
+    console.log('Hello MyCardsProvider Provider');
+  }
+
+  get():Observable<MyCard[]>{
+    let uri = dataMlab.baseMlabApi + '/collections/cards?apiKey='+dataMlab.apiKey;
+    let result = this.http.get<MyCard[]>(uri);
+    return this.cache.loadFromObservable(uri, result);
+  }
+
+  post(card: MyCard):Observable<any>{
+    let uri = dataMlab.baseMlabApi + '/collections/cards?apiKey='+dataMlab.apiKey;
+    return this.http.post(uri, JSON.stringify(card), { headers: { 'Content-Type': 'application/json' }});
+  }
+}
