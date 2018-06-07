0c907e72c5aa283b42c1bc6acc9eae362f6eeb9a
eduardo.florencio <eduardo.florencio@millennium.com.br>
2018-06-07 02:22:46 -0300

    Ajustes Visuais

--- src/pages/my-cards/my-cards.ts
+++ src/pages/my-cards/my-cards.ts
@@ -41,6 +41,16 @@ export class MyCardsPage {
     });
   }
 
+
+  doRefresh(refresher) { 
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyCardsPage');
   }
